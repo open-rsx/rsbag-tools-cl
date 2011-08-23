ff5374dd839ded23e3cc55527c9cbedfb2906e07
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-08-23 12:18:29 +0000

    Extended version display in bag-{record,play,info}/main.lisp * bag-record/main.lisp (main): report tools version, rsbag version and   rsbag TIDELog backend version * bag-play/main.lisp (main): likewise * bag-info/main.lisp (main): likewise

--- bag-play/main.lisp
+++ bag-play/main.lisp
@@ -49,6 +49,9 @@ Examples:
   (update-synopsis)
   (setf *default-configuration* (options-from-default-sources))
   (process-commandline-options
+   :version         (cl-rsbag-tools-play-system:version/list)
+   :more-versions   (list :rsbag         (cl-rsbag-system:version/list)
+			  :rsbag-tidelog (cl-rsbag-system:version/list))
    :update-synopsis #'update-synopsis
    :return          #'(lambda () (return-from main)))
 
