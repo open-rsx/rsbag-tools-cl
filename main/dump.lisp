4aa6fbe15a5f1bbbb779160605e06b2fd7ff2b58
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-03-09 15:13:21 +0000

    Do not fail if Spread library cannot be loaded in main/dump.lisp fixes #924 * main/dump.lisp (reload-spread-and-main): handle failures to load   Spread library by just warning and then continuing

--- main/dump.lisp
+++ main/dump.lisp
@@ -21,7 +21,12 @@
 (cffi:close-foreign-library 'spread::libspread)
 
 (defun reload-spread-and-main ()
-  (cffi:use-foreign-library spread::libspread)
+  (handler-case
+      (cffi:use-foreign-library spread::libspread)
+    (error (condition)
+      (warn "~@<Failed to load Spread library: ~A. Did you set ~
+LD_LIBRARY_PATH? ~_Spread transport will now be disabled.~@:>"
+	    condition)))
   (rsbag.tools.main:main))
 
 (com.dvlsoft.clon:dump "bag" reload-spread-and-main
