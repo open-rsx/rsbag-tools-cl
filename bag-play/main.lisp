7f3f29d0cc8e42e95be4efa6699c2279e2d71875
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-08-09 13:21:47 +0200

    Backport: Added toplevel error handling in bag-*/main.lisp refs #1022 * bag-cat/main.lisp (update-synopsis): call   `make-error-handling-options' to add error-handling-related   commandline options   (main): use `with-error-policy' * bag-info/main.lisp: similar * bag-merge/main.lisp: similar * bag-play/main.lisp: similar * bag-record/main.lisp (update-synopsis): similar   (main): use `with-error-policy; install error policy in the   connection

--- bag-play/main.lisp
+++ bag-play/main.lisp
@@ -26,6 +26,7 @@
    :postfix "INPUT-FILE [BASE-URI]"
    :item    (make-text :contents (make-help-string :show show))
    :item    (make-common-options :show show)
+   :item    (make-error-handling-options :show show)
    :item    (make-replay-options :show show)
    ;; Append RSB options.
    :item    (make-options
@@ -73,43 +74,47 @@ the value of `*info-output*'."
 
   (with-logged-warnings
 
-    ;; Create a reader and start the receiving and printing loop.
-    (bind (((input &optional (base-uri "/")) (remainder))
-	   (channel-specs    (iter (for channel next (getopt :long-name "channel"))
-				   (while channel)
-				   (collect channel)))
-	   (channels         (or (make-channel-filter channel-specs) t))
-	   ((:values start-time start-index end-time end-index)
-	    (process-bounds-options))
-	   (replay-strategy  (parse-instantiation-spec
-			      (getopt :long-name "replay-strategy")))
-	   (progress         (getopt :long-name "show-progress")))
+      ;; Create a reader and start the receiving and printing loop.
+      (bind ((error-policy (maybe-relay-to-thread
+			    (process-error-handling-options)))
+             ((input &optional (base-uri "/")) (remainder))
+	     (channel-specs    (iter (for channel next (getopt :long-name "channel"))
+				     (while channel)
+				     (collect channel)))
+	     (channels         (or (make-channel-filter channel-specs) t))
+	     ((:values start-time start-index end-time end-index)
+	      (process-bounds-options))
+	     (replay-strategy  (parse-instantiation-spec
+				(getopt :long-name "replay-strategy")))
+	     (progress         (getopt :long-name "show-progress")))
 
-      (log1 :info "Using ~:[~*all channels~;channels matching ~@<~@;~{~S~^, ~}~@:>~]"
-	    (not (eq channels t)) channel-specs)
-      (log1 :info "Using base-URI ~A" base-uri)
+	(log1 :info "Using ~:[~*all channels~;channels matching ~@<~@;~{~S~^, ~}~@:>~]"
+	      (not (eq channels t)) channel-specs)
+	(log1 :info "Using base-URI ~A" base-uri)
 
-      (let ((connection (apply #'bag->events input base-uri
-			       :channels        channels
-			       :replay-strategy replay-strategy
-			       (append (when start-time
-					 (list :start-time start-time))
-				       (when start-index
-					 (list :start-index start-index))
-				       (when end-time
-					 (list :end-time end-time))
-				       (when end-index
-					 (list :end-index end-index))))))
-
-	(log1 :info "Connection ~A" connection)
-
-	(unwind-protect
-	     (with-interactive-interrupt-exit ()
-	       (replay connection (connection-strategy connection)
-		       :progress (when *info-output*
-				   (case progress
-				     (:line #'print-progress)))))
-	  (close connection))
+	(with-interactive-interrupt-exit ()
+	  (with-error-policy (error-policy)
+	    (let ((connection (apply #'bag->events input base-uri
+				     :channels        channels
+				     :replay-strategy replay-strategy
+				     (append
+				      (when start-time
+					(list :start-time start-time))
+				      (when start-index
+					(list :start-index start-index))
+				      (when end-time
+					(list :end-time end-time))
+				      (when end-index
+					(list :end-index end-index))))))
+	      (setf (rsb.ep:processor-error-policy connection)
+		    error-policy)
+	      (log1 :info "Connection ~A" connection)
+	      (unwind-protect
+		   (replay connection (connection-strategy connection)
+			   :progress (when *info-output*
+				       (case progress
+					 (:line #'print-progress))))
+		(close connection)))))
 
 	(unless (eq progress :none)
-	  (terpri *standard-output*))))))
+	  (terpri *standard-output*)))))
