a1f60216eaa82af5c8001caf2f14a5a7a09cee5e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-02-15 20:22:05 +0100

    Load network.spread instead of cl-spread in main/dump.lisp
    refs #1170
    
    * main/dump.lisp: load system network.spread instead of cl-spread; use
      `network.spread:enable-reload-spread-library' instead of
      unloading/reloading the Spread library directly

--- main/dump.lisp
+++ main/dump.lisp
@@ -2,13 +2,13 @@
 
 (setf iterate::*always-declare-variables* t)
 
-(load-system :cl-ppcre)    ;; for regex filter
+(load-system :cl-ppcre)       ;; for regex filter
 
 (load-system :cl-protobuf)
-(load-system :cl-spread)   ;; for spread transport
-(load-system :usocket)     ;; for socket transport
+(load-system :network.spread) ;; for spread transport
+(load-system :usocket)        ;; for socket transport
 
-(load-system :swank)       ;; for the lulz
+(load-system :swank)          ;; for the lulz
 
 (unless (asdf:find-system :cl-rsb-common nil)
   (ignore-errors (load-system :cl-rsb-tools)))
@@ -18,18 +18,14 @@
 (load-system :cl-rsbag-tidelog)
 (load-system :cl-rsbag-elan)
 
+;;; Image saving/resuming logistics
+
+;; Reset ASDF state
 (asdf:clear-source-registry)
 (asdf:clear-output-translations)
-(cffi:close-foreign-library 'spread::libspread)
 
-(defun reload-spread-and-main ()
-  (handler-case
-      (cffi:use-foreign-library spread::libspread)
-    (error (condition)
-      (warn "~@<Failed to load Spread library: ~A. Did you set ~
-LD_LIBRARY_PATH? ~_Spread transport will now be disabled.~@:>"
-	    condition)))
-  (rsbag.tools.main:main))
+;; Try to reload Spread library
+(network.spread:enable-reload-spread-library :if-fails #'warn)
 
-(com.dvlsoft.clon:dump "bag" reload-spread-and-main
+(com.dvlsoft.clon:dump "bag" rsbag.tools.main:main
 		       :compression :best)
