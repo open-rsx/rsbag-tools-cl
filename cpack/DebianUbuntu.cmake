972341d92617a738fd950f97e97a57c58848dc86
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-03-06 10:30:53 +0100

    "Unscreamified" CMakeLists.txt, cpack/{DebianUbuntu,CPackInclude}.cmake
    * CMakeLists.txt: unscreamified
      (header): updated copyright
    * cpack/DebianUbuntu.cmake: unscreamified
    * cpack/CPackInclude.cmake: likewise

--- cpack/DebianUbuntu.cmake
+++ cpack/DebianUbuntu.cmake
@@ -1,69 +1,69 @@
-SET(CPACK_GENERATOR "DEB")
+set(CPACK_GENERATOR "DEB")
 
-SET(CPACK_DEBIAN_PACKAGE_NAME        "${PACKAGE_BASE_NAME}")
-SET(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
-SET(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Jan Moringen <jmoringe@techfak.uni-bielefeld.de>")
-SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Tools for recording and replaying RSB events (Common Lisp implementation)
+set(CPACK_DEBIAN_PACKAGE_NAME        "${PACKAGE_BASE_NAME}")
+set(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Jan Moringen <jmoringe@techfak.uni-bielefeld.de>")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Tools for recording and replaying RSB events (Common Lisp implementation)
  Currently, the following tools are available
   * bag-record
   * bag-info
   * bag-play
   * bag-cat
   * bag-merge")
-SET(CPACK_DEBIAN_PACKAGE_PRIORITY    "optional")
-SET(CPACK_DEBIAN_PACKAGE_SECTION     "net")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY    "optional")
+set(CPACK_DEBIAN_PACKAGE_SECTION     "net")
 #SET(CPACK_DEBIAN_ARCHITECTURE        "${CMAKE_SYSTEM_PROCESSOR}") # Debian uses different names here
-SET(CPACK_DEBIAN_PACKAGE_DEPENDS     "libc6")
-SET(CPACK_DEBIAN_PACKAGE_RECOMMENDS  "spread (>= 4.0)")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS     "libc6")
+set(CPACK_DEBIAN_PACKAGE_RECOMMENDS  "spread (>= 4.0)")
 
 # Generate postinst and prerm hooks
-SET(PACKAGE_ALT_PRIORITY "100")
+set(PACKAGE_ALT_PRIORITY "100")
 
-SET(POSTINST_SCRIPT      "${CMAKE_CURRENT_BINARY_DIR}/postinst")
-SET(PRERM_SCRIPT         "${CMAKE_CURRENT_BINARY_DIR}/prerm")
-FILE(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
-FILE(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
-FOREACH(TOOL ${TOOLS})
-    FILE(APPEND "${POSTINST_SCRIPT}"
+set(POSTINST_SCRIPT      "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+set(PRERM_SCRIPT         "${CMAKE_CURRENT_BINARY_DIR}/prerm")
+file(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
+file(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
+foreach(TOOL ${TOOLS})
+    file(APPEND "${POSTINST_SCRIPT}"
                 "update-alternatives --install                       \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}                  \\
                    ${BINARY_PREFIX}${TOOL}                           \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}${VERSION_SUFFIX} \\
                    ${PACKAGE_ALT_PRIORITY}\n\n")
-    FILE(APPEND "${PRERM_SCRIPT}"
+    file(APPEND "${PRERM_SCRIPT}"
                 "update-alternatives --remove                            \\
                    ${BINARY_PREFIX}${TOOL}                               \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}${VERSION_SUFFIX}\n\n")
-ENDFOREACH()
-EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}")
-SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${POSTINST_SCRIPT};${PRERM_SCRIPT}")
+endforeach()
+execute_process(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}")
+set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${POSTINST_SCRIPT};${PRERM_SCRIPT}")
 
 # Generate required change log files.
-FIND_PROGRAM(LSB_EXECUTABLE "lsb_release")
-EXECUTE_PROCESS(COMMAND ${LSB_EXECUTABLE} --short --codename
+find_program(LSB_EXECUTABLE "lsb_release")
+execute_process(COMMAND ${LSB_EXECUTABLE} --short --codename
                 OUTPUT_VARIABLE LSB_CODENAME
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+execute_process(COMMAND ${GIT_EXECUTABLE}
                         log "--format=%ad  %an  <%ae>%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n"
                         --date=short
                 COMMAND gzip -9
                 OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.gz")
-EXECUTE_PROCESS(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
+execute_process(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
                 OUTPUT_VARIABLE COMMIT_TO_VERSION_SED_RULES)
-EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+execute_process(COMMAND ${GIT_EXECUTABLE}
                         log "--format=${CPACK_DEBIAN_PACKAGE_NAME} (%H) ${LSB_CODENAME}; urgency=low%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n%n%w(200,1,1)-- %an <%ae>  %ad%n"
                         --date=rfc
                         -- "${CMAKE_CURRENT_LIST_FILE}"
                 COMMAND sh -c ${COMMIT_TO_VERSION_SED_RULES}
                 COMMAND gzip -9
                 OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.Debian.gz")
-INSTALL(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
+install(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
               "${CMAKE_BINARY_DIR}/changelog.Debian.gz"
         DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
 
 # Write license file
-FILE(WRITE "${CMAKE_BINARY_DIR}/copyright"
+file(WRITE "${CMAKE_BINARY_DIR}/copyright"
      "Copyright (C) 2011-2012 ${CPACK_DEBIAN_PACKAGE_MAINTAINER}
 
    This software may be licensed under the terms of the GNU General
@@ -82,9 +82,9 @@ FILE(WRITE "${CMAKE_BINARY_DIR}/copyright"
 
 On Debian systems, the complete text of the GNU General Public License
 can be found in `/usr/share/common-licenses/GPL-3'.")
-INSTALL(FILES "${CMAKE_BINARY_DIR}/copyright"
+install(FILES "${CMAKE_BINARY_DIR}/copyright"
         DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
 
-SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
 
-MESSAGE(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
+message(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
