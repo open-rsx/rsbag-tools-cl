e82eb2520b9dec7e5809192b4438d65df3c360bd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-10-07 15:49:14 +0200

    Shortened license, updated copyright in **/*.lisp, cl-rsbag-*.asd, CMakeLists.txt
    * **/*.lisp (header): shortened license; updated copyright
    * cl-rsbag-common.asd: likewise
    * cl-rsbag-tools-main.asd: likewise
    * cl-rsbag-tools-cat.asd: likewise
    * cl-rsbag-tools-info.asd: likewise
    * cl-rsbag-tools-merge.asd: likewise
    * cl-rsbag-tools-play.asd: likewise
    * cl-rsbag-tools-record.asd: likewise
    * CMakeLists.txt: likewise

--- scripts/export-audio-as-wav.lisp
+++ scripts/export-audio-as-wav.lisp
@@ -1,24 +1,12 @@
-;;; export-audio-as-wav.lisp --- Export rst.audition.SoundChunk as WAV stream.
-;;
-;; Copyright (C) 2012, 2013 Jan Moringen
-;;
-;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
-;;
-;; This Program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This Program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses>.
+;;;; export-audio-as-wav.lisp --- Export rst.audition.SoundChunk as WAV stream.
+;;;;
+;;;; Copyright (C) 2012, 2013 Jan Moringen
+;;;;
+;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
-;; Note: this script exists mainly for educational purposes. In most
-;; cases, the :audio-stream/wav formatting style can be used instead.
+;;;; Note: this script exists mainly for educational purposes. In most
+;;;; cases, the :audio-stream/wav formatting style can be used
+;;;; instead.
 
 #.(unless (when-let* ((package (find-package "RST.AUDITION"))
 		      (symbol  (find-symbol "SOUND-CHUNK-CHANNELS" package)))
