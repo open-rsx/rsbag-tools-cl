94466d85b907208a708342ef826f6106534b2081
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-09-06 23:31:31 +0200

    Fixed progress format control in src/commands/util.lisp
    * src/commands/util.lisp (print-replay-progress): fixed format control
      string

--- src/commands/util.lisp
+++ src/commands/util.lisp
@@ -10,7 +10,7 @@
                               progress index start-index end-index timestamp)
   ;; Print the progress of the current replay onto STREAM.
   (when (and progress index start-index end-index)
-    (format stream "~C~:[N/A~:;~:*~A~] ~:*~6,2,2F % ~9:D [~9:D,~9:D]"
+    (format stream "~C~:[N/A~:;~:*~A~] ~6,2,2F % ~9:D [~9:D,~9:D]"
             #\Return timestamp progress index start-index end-index)
     (force-output stream)))
 
